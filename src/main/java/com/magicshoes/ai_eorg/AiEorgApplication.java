3904cbb9e525b05c9467fcbc24ec3fadb9c9feb8
Mike McLaughlin <mike@magicshoes.net>
2025-03-26 02:31:25 +0000

    Intialzr import

---
+++ src/main/java/com/magicshoes/ai_eorg/AiEorgApplication.java
@@ -0,0 +1,13 @@
+package com.magicshoes.ai_eorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEorgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEorgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicshoes/ai_eorg/AiEorgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicshoes.ai_eorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEorgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
