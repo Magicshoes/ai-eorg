25df4a05f46852e33b61da14dd65bce406f0897c
Mike McLaughlin <mike@magicshoes.net>
2025-04-06 16:11:09 +0000

    Feat: added bearer token, security, and basic tests

--- src/test/java/com/magicshoes/ai_eorg/AiEorgApplicationTests.java
+++ src/test/java/com/magicshoes/ai_eorg/AiEorgApplicationTests.java
@@ -2,11 +2,17 @@ package com.magicshoes.ai_eorg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class AiEorgApplicationTests {
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
+	@WithMockUser(username = "testuser", roles = {"USER"})
 	void contextLoads() {
 	}
 
